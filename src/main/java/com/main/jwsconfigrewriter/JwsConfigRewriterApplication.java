4fc7116e426fe92ac0974a327ce8fe5913cda3ea
Denis.Viskov <Denis.Viskov@nordigy.ru>
2021-09-12 11:22:28 +0300

    some manipulations

--- src/main/java/com/main/jwsconfigrewriter/JwsConfigRewriterApplication.java
+++ src/main/java/com/main/jwsconfigrewriter/JwsConfigRewriterApplication.java
@@ -1,13 +1,18 @@
 package com.main.jwsconfigrewriter;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class JwsConfigRewriterApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JwsConfigRewriterApplication.class, args);
+        log.info(" --------------------------------------------------------------------------------------");
+        log.info("|                                JWS-CONFIG-REWRITER V1.0                              |");
+        log.info(" --------------------------------------------------------------------------------------");
     }
 
 }
